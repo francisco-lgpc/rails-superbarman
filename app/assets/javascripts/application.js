f785c4b1c183e04d8f3c12d2c1eb4f64176f0f45
laurens de vries <leh.devries@gmail.com>
2017-04-26 13:33:41 +0200

    added pics to cards and some css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+
 //= require_tree .
+
+// $("input.datepicker").each(function(input) {
+//   $(this).datepicker({
+//     dateFormat: "yy-mm-dd",
+//     altField: $(this).next()
+//   })
+// })
+  // If you use i18n-js you can set the locale like that
+  // $(this).datepicker("option", $.datepicker.regional[I18n.currentLocale()]);
+
