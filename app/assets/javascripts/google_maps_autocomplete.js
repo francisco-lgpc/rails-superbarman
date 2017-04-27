ad39dc95503d5e386246ad195f2d267e44bc27d1
laurens de vries <leh.devries@gmail.com>
2017-04-27 14:48:38 +0200

    spent half my life getting geo shizzle to work

--- app/assets/javascripts/google_maps_autocomplete.js
+++ app/assets/javascripts/google_maps_autocomplete.js
@@ -1,14 +1,15 @@
-$(document).ready(function() {
-  var bartender_address = $('#bartender_address').get(0);
+// $(document).ready(function() {
+//   var party_address = $('#party_address').get(0);
 
-  if (bartender_address) {
-    var autocomplete = new google.maps.places.Autocomplete(bartender_address, { types: ['geocode'] });
-    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-    google.maps.event.addDomListener(bartender_address, 'keydown', function(e) {
-      if (e.keyCode == 13) {
-        e.preventDefault(); // Do not submit the form on Enter.
-      }
-    });
-  }
-});
-
+//   if (party_address) {
+//     console.log(party_address)
+//     console.log(google)
+//     var autocomplete = new google.maps.places.Autocomplete(party_address, { types: ['geocode'] });
+// //    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+//     google.maps.event.addDomListener(party_address, 'keydown', function(e) {
+//       if (e.keyCode == 13) {
+//         e.preventDefault(); // Do not submit the form on Enter.
+//       }
+//     });
+//   }
+// });
