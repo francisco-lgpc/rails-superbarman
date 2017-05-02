42520ae7f41c00978692009eabac36970ac3c70b
Francisco Coutinho <francisco.lgpc@gmail.com>
2017-05-02 18:41:17 +0200

    bartender is a devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 
 Rails.application.routes.draw do
 
+  devise_for :bartenders, skip: :registerable
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+  get '/log_in', to: 'pages#sign_in'
   post '/bartenders/search', to: 'bartenders#search'
 
   get '/bartenders/search', to: 'bartenders#search'
