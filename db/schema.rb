cf921bb70dbbe3f2bee78ef4c76985a9c755da08
Daniel Huertas <danielhuertascompany@gmail.com>
2017-05-02 12:42:17 +0200

    Added User to Review Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502102416) do
+ActiveRecord::Schema.define(version: 20170502103713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,9 @@ ActiveRecord::Schema.define(version: 20170502102416) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "party_id"
+    t.integer  "user_id"
     t.index ["party_id"], name: "index_reviews_on_party_id", using: :btree
+    t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -90,4 +92,5 @@ ActiveRecord::Schema.define(version: 20170502102416) do
   add_foreign_key "parties", "bartenders"
   add_foreign_key "parties", "users"
   add_foreign_key "reviews", "parties"
+  add_foreign_key "reviews", "users"
 end
