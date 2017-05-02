fe331c14be49cf665ef0001df3cdd87abcea3f87
laurens de vries <leh.devries@gmail.com>
2017-05-02 15:55:33 +0200

    took out them validation so it doesnt break when hitting book if person hasnt specified the theme

--- app/models/party.rb
+++ app/models/party.rb
@@ -2,5 +2,5 @@ class Party < ApplicationRecord
   TYPES = ['Party', 'Birthday', 'Hen/Stag do', 'Leaving do', 'House Warming', 'Wedding']
   belongs_to :user
   belongs_to :bartender
-  validates :theme, inclusion: { in: TYPES }
+  # validates :theme, inclusion: { in: TYPES }
 end
