2230b70886bda1b8d65d427299519d20c31ab90c
Francisco Coutinho <francisco.lgpc@gmail.com>
2017-05-09 20:05:07 +0200

    payment new is now nested in party new

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,26 +6,25 @@ before_action :set_party
 
   def create
     customer = Stripe::Customer.create(
-      source: params[:stripeToken],
-      email:  params[:stripeEmail]
+      email:  current_user.email,
+      source: params.require(:token)[:id]
     )
 
-  charge = Stripe::Charge.create(
-    customer:     customer.id,
-    amount:       @party.price_cents,
-    description:  "Payment for #{@party.theme} - #{@party.bartender.name}",
-    currency:     @party.price.currency
-  )
+    charge = Stripe::Charge.create(
+      customer:     customer.id,
+      amount:       @party.price_cents,
+      description:  "Payment for #{@party.theme} - #{@party.bartender.name}",
+      currency:     @party.price.currency
+    )
 
-  @party.update(payment: charge.to_json, payment_confirmed: 'true')
-  redirect_to party_path(@party)
+    @party.update(payment: charge.to_json, payment_confirmed: 'true')
 
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_party_payment_path(@party)
-  end
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_party_payment_path(@party)
+    end
 
-private
+  private
 
   def set_party
     @party = Party.where(payment_confirmed: 'false').find(params[:party_id])
