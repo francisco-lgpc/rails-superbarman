2bb4579844818874e72545f5f3319426eb35dbaa
Daniel Huertas <danielhuertascompany@gmail.com>
2017-05-09 16:31:45 +0200

    icon added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+    config.relative_url_root = ""
   # When you add controller actions that should be visible to everyone,
   # don't forge to add this
   # 'skip_before_action :authenticate_user!, only: [:some_action, :another_action, etc...]'
