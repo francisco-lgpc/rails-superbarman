abbb38bf37a6f721fb448cabf253c64f7bb74dbd
Daniel Huertas <danielhuertascompany@gmail.com>
2017-05-10 13:30:58 +0200

    helpers

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,14 +1,14 @@
 module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["Super Barman  - Hire a bartender for your party for only 50€."]
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
   end
 
   def meta_description
-    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["Bring the bar to you and serve the best ever cocktails thanks to our curated bartenders. Your parties will never be the same again."]
+    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
   end
 
   def meta_image
-    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["banner.png"])
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
     # little twist to make it work equally with an asset or a url
     meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
   end
